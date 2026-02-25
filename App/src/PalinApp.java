87a9962ff0c39693ac2e8f793ead3c37b757526f
unknown <kp8447@srmist.edu.in>
2026-02-25 09:23:13 +0530

    uc1 commited

---
+++ App/src/PalinApp.java
@@ -0,0 +1,7 @@
+public class PalinApp {
+    public static void main(String[] args) {
+        System.out.println("Pallindrome app");
+        System.out.println("version 1.o");
+        System.out.println("system initialised successfully");
+    }
+}
